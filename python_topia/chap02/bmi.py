6eb4ed2907adffddebb7116c8a93b69382c68112
tmyk-ykmz <tmyk.ykmz@gmail.com>
2020-01-19 21:46:13 +0900

    2020/01/19 test

---
+++ python_topia/chap02/bmi.py
@@ -0,0 +1,22 @@
+# BMI判定プログラム
+weight = float(input("体重(kg)は？"))
+height = float(input("身長(cm)は？"))
+
+# BMIの計算
+height = height / 100 # (m)に直す
+bmi = weight / (height * height)
+
+# BMIの値に応じて結果を分岐
+result = "" # resultを空にしておく
+if bmi < 18.5:
+    result = "痩せ型"
+if (18.5 <= bmi) and (bmi < 25):
+    result = "標準体型"
+if (25 <= bmi) and (bmi < 30):
+    result = "肥満(軽)"
+if bmi >= 30:
+    result = "肥満(重)"
+
+# 結果を表示
+print("BMI :", bmi)
+print("判定:", result)
---
+++ python_topia/chap02/even-odd.py
@@ -0,0 +1,5 @@
+num = int(input("数字は？"))
+if num % 2 == 0:
+    print("偶数です")
+else:
+    print("奇数です")
