962f687ff195643a8d971a4a08051ace804688a7
tmyk-ykmz <tmyk.ykmz@gmail.com>
2020-01-20 14:17:05 +0900

    2020/01/20 chap2-6

---
+++ python_topia/chap02/tubo2.py
@@ -0,0 +1,10 @@
+#  繰り返し坪数を調べるプログラム
+while True:
+    user = input("坪数は？")
+
+    # 「q」を入力するとプログラムが停止する
+    if user == "" or user == "q": break #修正点
+    tubo = int(user)
+    m2 = tubo * 3.31
+    s = "{0}坪は　{1}平方メートルです".format(tubo, m2)
+    print(s)
