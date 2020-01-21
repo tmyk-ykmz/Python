6d318204baedfcccc6eb549dbe5c24457a21ef65
tmyk-ykmz <tmyk.ykmz@gmail.com>
2020-01-21 14:38:54 +0900

    2020/01/21 python

---
+++ python_topia/chap02/simple.py
@@ -0,0 +1,14 @@
+# 繰り返し中止のbreakと継続のcontinue
+
+# break
+for i in range(10):
+    print(i)
+    if i == 2 : break
+
+"""
+# continue
+for i in range(5):
+    print("i=",i)
+    if i >= 3 : continue
+    print("- hello!")
+"""
