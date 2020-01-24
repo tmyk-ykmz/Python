a1927c8fc1f06ab2a68fddcaed868d82106c4903
tmyk-ykmz <tmyk.ykmz@gmail.com>
2020-01-24 14:34:31 +0900

    2020/01/24 chap03

--- python_topia/chap03/count-words.py
+++ python_topia/chap03/count-words.py
@@ -6,3 +6,23 @@ keep on knocking, and it will be opened to you;
 for everyone asking receives, and everyone seeking finds,
 and to everyone knocking, it will be opened.
 """
+
+# 単語を区切る
+text = text.replace(";", "")    # ;を削除
+text = text.replace(",", "")    # ,を削除
+text = text.replace(".", "")    # .を削除
+words = text.split()    # 空白で区切ってリスト型を作成
+
+# 単語を数える
+counter = {}    # counterという空の辞書型を作成
+for w in words:
+    ws = w.lower() #小文字に変換
+    if ws in counter:   # もし辞書型にすでにキーがあれば値を1つ追加
+        counter[ws] += 1
+    else:
+        counter[ws] = 1    # もし辞書型にキーがなければ、値を1としてキーも登録
+
+# 結果を表示
+for k,v in sorted(counter.items()): # counterのキーをアルファベット順として範囲に指定
+    if v >= 3:
+        print(k, v)
